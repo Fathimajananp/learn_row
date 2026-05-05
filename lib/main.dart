2a1d23400b3941cdc38ba9888baf2c3a31a5f048
Fathima Janan P <janananshad@gmail.com>
2026-05-05 16:10:25 +0530

    learn_row

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('Deliver features faster',
+                          textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      child: Text(
+                          'Craft beautiful UIs', textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      child: FittedBox(
+                        child: FlutterLogo(),
+                      ),
+                    ),
+                  ],
+                )
+            )
+        )
+    );
+  }
+}
+
